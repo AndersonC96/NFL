4b8cbec66ed4223f6722ab0b6b1bc5e7d80d3cf5
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2021-08-05 13:17:17 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,11 +8,11 @@
 <?php echo "Welcome: "  . $_SESSION['logado']['nome']; ?>
 <section class="jumbotron text-center">
     <div class="container">
-        <h1 class="jumbotron-heading">CRUD | NFL</h1>
-        <p class="lead text-muted">Application to register, view, edit and delete new Players from the team.</p>
-        <p class="lead text-muted">Click on the Menu above and do it right now!</p>
+        <h1 class="jumbotron-heading">NFL</h1>
+        <p class="lead text-muted">Site para cadastrar, visualizar, editar e deletar novos Jogadores, Super Bowl, MVP e Times.</p>
+        <p class="lead text-muted">Clique no Menu acima e faça isso agora mesmo!</p>
         <p><img src="images/NFL_Logo.png"  style="width: 400px;"></p>
-        <p><a href="https://www.nfl.com/">Official NFL Website</></p>
+        <p><a href="https://www.nfl.com/">Site Oficial da NFL</></p>
     </div>
 </section>
 <?php require_once 'template/rodape.php'; ?>
