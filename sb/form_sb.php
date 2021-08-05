4eb218f743fea450f1822639aac153a22885bbd2
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2021-08-05 13:33:25 -0300

    sb update

--- sb/form_sb.php
+++ sb/form_sb.php
@@ -5,41 +5,41 @@
 <div class="container">
     <form class="" action="<?php echo $acao; ?>" method="post">
         <div class="from-group">
-            <label for="nome">Name</label>
+            <label for="nome">Nome</label>
             <input id="nome" class="form-control" type="text" name="nome"
                 value="<?php if(isset($registro)) echo $registro['nome']; ?>" required>
         </div>
         <div class="from-group">
-            <label for="data">Date</label>
+            <label for="data">Data</label>
             <input id="data" class="form-control" type="date" name="data"
                 value="<?php if(isset($registro)) echo $registro['data']; ?>" maxlength="500" required>
         </div>
         <div class="from-group">
-            <label for="nome">Champion</label>
+            <label for="nome">Campeão</label>
             <input id="nome" class="form-control" type="text" name="campeao"
                 value="<?php if(isset($registro)) echo $registro['campeao']; ?>" required>
         </div>
         <div class="from-group">
-            <label for="nome">Venue</label>
+            <label for="nome">Estádio</label>
             <input id="nome" class="form-control" type="text" name="estadio"
                 value="<?php if(isset($registro)) echo $registro['estadio']; ?>" required>
         </div>
         <div class="from-group">
-            <label for="nome">City</label>
+            <label for="nome">Cidade</label>
             <input id="nome" class="form-control" type="text" name="cidade"
                 value="<?php if(isset($registro)) echo $registro['cidade']; ?>" required>
         </div>
         <div class="from-group">
-            <label for="nome">Attendance</label>
+            <label for="nome">Público</label>
             <input id="nome" class="form-control" type="text" name="publico"
                 value="<?php if(isset($registro)) echo $registro['publico']; ?>" required>
         </div>
         <div class="from-group">
-            <label for="nome">Referee</label>
+            <label for="nome">Árbitro</label>
             <input id="nome" class="form-control" type="text" name="juiz"
                 value="<?php if(isset($registro)) echo $registro['juiz']; ?>" required>
         </div>
         <br>
-        <button class="btn btn-info" type="submit">Send</button>
+        <button class="btn btn-info" type="submit">Enviar</button>
     </form>
 </div>
--- sb/lista_sb.php
+++ sb/lista_sb.php
@@ -1,26 +1,24 @@
 <div class="container print">
     <h2>Super Bowl</h2>
-    <p>Register all Super Bowl data.</p>
-    <a class="btn btn-info" href="sb.php?acao=novo">New</a>
+    <p>Registre todos os dados do Super Bowl</p>
+    <a class="btn btn-info" href="sb.php?acao=novo">Novo</a>
     <?php if (count($registros)==0): ?>
-    <p>No records found.</p>
+    <p>Sem registros encontrados</p>
     <?php else: ?>
     <table class="table table-hover table-stripped">
         <thead>
-            <th>#</th>
-            <th>Name</th>
-            <th>Date</th>
-            <th>Champion</th>
-            <th>Venue</th>
-            <th>City</th>
-            <th>Attendance</th>
-            <th>Referee</th>
-            <th>Actions</th>
+            <th>Nome</th>
+            <th>Data</th>
+            <th>Campeão</th>
+            <th>Estádio</th>
+            <th>Cidade</th>
+            <th>Público</th>
+            <th>Árbitro</th>
+            <th>Ações</th>
         </thead>
         <tbody>
             <?php foreach ($registros as $linha): ?>
             <tr>
-                <td><?= $linha['id']; ?></td>
                 <td><?= $linha['nome']; ?></td>
                 <td><?= $linha['data']; ?></td>
                 <td><?= $linha['campeao']; ?></td>
@@ -29,8 +27,8 @@
                 <td><?php echo $linha['publico']; ?></td>
                 <td><?php echo $linha['juiz']; ?></td>
                 <td>
-                    <a class="btn btn-warning btn-sm" href="sb.php?acao=buscar&id=<?php echo $linha['id']; ?>">Edit</a>
-                    <a class="btn btn-danger btn-sm" href="sb.php?acao=excluir&id=<?php echo $linha['id']; ?>">Delete</a>
+                    <a class="btn btn-warning btn-sm" href="sb.php?acao=buscar&id=<?php echo $linha['id']; ?>">Editar</a>
+                    <a class="btn btn-danger btn-sm" href="sb.php?acao=excluir&id=<?php echo $linha['id']; ?>">Excluir</a>
                 </td>
             </tr>
             <?php endforeach; ?>
