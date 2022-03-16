239a214d9c621cde1150689b4bc7353e78fafd7e
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2022-03-16 17:10:56 -0300

    Update posicao.php

--- posicao/posicao.php
+++ posicao/posicao.php
@@ -1,76 +1,74 @@
 <?php
-    session_start();
-    if(!isset($_SESSION['logado'])){
-        header('Location: login.php');
+    session_start();// iniciar sessão
+    if(!isset($_SESSION['logado'])){// se não existir a sessão
+        header('Location: login.php');// redirecionar para a página de login
     }
 ?>
 <?php
-    require_once '../config/conexao.php';
-    if(!isset($_GET['acao'])) $acao="listar";
-    else $acao = $_GET['acao'];
-    if($acao=="listar"){
-        $sql   = "SELECT p.id, p.nome, c.nome as classe
-                    FROM posicao p
-                    Inner Join classe c ON c.id=p.id_classe";
-        $query = $con->query($sql);
-        $registros = $query->fetchAll();
-        require_once '../template/cabecalho.php';
-        require_once 'lista_posicao.php';
-        require_once '../template/rodape.php';
-    }else if($acao == "novo"){
-        $lista_classe = getClasses();
-        require_once '../template/cabecalho.php';
-        require_once 'form_posicao.php';
-        require_once '../template/rodape.php';
-    }else if($acao == "gravar"){
-        $registro = $_POST;
-        $sql = "INSERT INTO posicao(nome, id_classe) VALUES(:nome, :id_classe)";
-        $query = $con->prepare($sql);
-        $result = $query->execute($registro);
-        if($result){
-            header('Location: ./posicao.php');
-        }else{
-            echo "Erro ao tentar inserir o registro, mensagem:". print_r($query->errorInfo());
+    require_once '../config/conexao.php';// conexão com o banco de dados
+    if(!isset($_GET['acao'])) $acao="listar";// se não existir a ação
+    else $acao = $_GET['acao'];// armazenar a ação
+    if($acao=="listar"){// se a ação for listar
+        $sql   = "SELECT p.id, p.nome, c.nome as classe FROM posicao p INNER JOIN classe c ON c.id=p.id_classe";// consulta ao banco de dados
+        $query = $con->query($sql);// preparar a consulta
+        $registros = $query->fetchAll();// armazenar os registros
+        require_once '../template/cabecalho.php';// carregar o cabeçalho
+        require_once 'lista_posicao.php';// carregar a lista de posições
+        require_once '../template/rodape.php';// carregar o rodapé
+    }else if($acao == "novo"){// se a ação for novo
+        $lista_classe = getClasses();// lista de classes
+        require_once '../template/cabecalho.php';// carregar o cabeçalho
+        require_once 'form_posicao.php';// carregar o formulário de posição
+        require_once '../template/rodape.php';// carregar o rodapé
+    }else if($acao == "gravar"){// se a ação for gravar
+        $registro = $_POST;// armazenar os dados do formulário
+        $sql = "INSERT INTO posicao(nome, id_classe) VALUES(:nome, :id_classe)";// inserir no banco de dados
+        $query = $con->prepare($sql);// preparar a consulta
+        $result = $query->execute($registro);// executar a consulta
+        if($result){// se o resultado for verdadeiro
+            header('Location: ./posicao.php');// redirecionar para a página de posições
+        }else{// se o resultado for falso
+            echo "Erro ao tentar inserir o registro, mensagem:". print_r($query->errorInfo());// exibir a mensagem de erro
         }
-    }else if($acao == "excluir"){
-        $id    = $_GET['id'];
-        $sql   = "DELETE FROM posicao WHERE id = :id";
-        $query = $con->prepare($sql);
-        $query->bindParam(':id', $id);
-        $result = $query->execute();
-        if($result){
-            header('Location: ./posicao.php');
-        }else{
-            echo "Erro ao tentar remover o registro de id: " . $id;
+    }else if($acao == "excluir"){// se a ação for excluir
+        $id    = $_GET['id'];// armazenar o id do registro
+        $sql   = "DELETE FROM posicao WHERE id = :id";// excluir do banco de dados
+        $query = $con->prepare($sql);// preparar a consulta
+        $query->bindParam(':id', $id);// vincular o id
+        $result = $query->execute();// executar a consulta
+        if($result){// se o resultado for verdadeiro
+            header('Location: ./posicao.php');// redirecionar para a página de posições
+        }else{// se o resultado for falso
+            echo "Erro ao tentar remover o registro de id: " . $id;// exibir a mensagem de erro
         }
-    }else if($acao == "buscar"){
-        $lista_classe = getClasses();
-        $id    = $_GET['id'];
-        $sql   = "SELECT * FROM posicao WHERE id = :id";
-        $query = $con->prepare($sql);
-        $query->bindParam(':id', $id);
-        $query->execute();
-        $registro = $query->fetch();
-        require_once '../template/cabecalho.php';
-        require_once 'form_posicao.php';
-        require_once '../template/rodape.php';
-    }else if($acao == "atualizar"){
-        $sql   = "UPDATE posicao SET nome = :nome, id_classe = :id_classe WHERE id = :id";
-        $query = $con->prepare($sql);
-        $query->bindParam(':id', $_GET['id']);
-        $query->bindParam(':nome', $_POST['nome']);
-        $query->bindParam(':id_classe', $_POST['id_classe']);
-        $result = $query->execute();
-        if($result){
-            header('Location: ./posicao.php');
-        }else{
-            echo "Error trying to update data" . print_r($query->errorInfo());
+    }else if($acao == "buscar"){// se a ação for buscar
+        $lista_classe = getClasses();// lista de classes
+        $id    = $_GET['id'];// armazenar o id do registro
+        $sql   = "SELECT * FROM posicao WHERE id = :id";// consulta ao banco de dados
+        $query = $con->prepare($sql);// preparar a consulta
+        $query->bindParam(':id', $id);// vincular o id
+        $query->execute();// executar a consulta
+        $registro = $query->fetch();// armazenar o registro
+        require_once '../template/cabecalho.php';// carregar o cabeçalho
+        require_once 'form_posicao.php';// carregar o formulário de posição
+        require_once '../template/rodape.php';// carregar o rodapé
+    }else if($acao == "atualizar"){// se a ação for atualizar
+        $sql   = "UPDATE posicao SET nome = :nome, id_classe = :id_classe WHERE id = :id";// atualizar no banco de dados
+        $query = $con->prepare($sql);// preparar a consulta
+        $query->bindParam(':id', $_GET['id']);// vincular o id
+        $query->bindParam(':nome', $_POST['nome']);// vincular o nome
+        $query->bindParam(':id_classe', $_POST['id_classe']);// vincular a classe
+        $result = $query->execute();// executar a consulta
+        if($result){// se o resultado for verdadeiro
+            header('Location: ./posicao.php');// redirecionar para a página de posições
+        }else{// se o resultado for falso
+            echo "Erro ao tentar atualizar os dados" . print_r($query->errorInfo());// exibir a mensagem de erro
         }
     }
-    function getClasses(){
-        $sql   = "SELECT * FROM classe";
-        $query = $GLOBALS['con']->query($sql);
-        $lista_classe = $query->fetchAll();
-        return $lista_classe;
+    function getClasses(){// função para listar as classes
+        $sql   = "SELECT * FROM classe";// consulta ao banco de dados
+        $query = $GLOBALS['con']->query($sql);// preparar a consulta
+        $lista_classe = $query->fetchAll();// armazenar os registros
+        return $lista_classe;// retornar a lista de classes
     }
 ?>
