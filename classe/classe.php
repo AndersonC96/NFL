33754d472c562aa6341cffb22f468ed4afd87dab
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2022-03-16 17:02:51 -0300

    Update classe.php

--- classe/classe.php
+++ classe/classe.php
@@ -1,65 +1,65 @@
 <?php
-    session_start();
-    if(!isset($_SESSION['logado'])){
-        header('Location: login.php');
+    session_start();// iniciar sessão
+    if(!isset($_SESSION['logado'])){// se não existir a sessão
+        header('Location: login.php');// redirecionar para a página de login
     }
 ?>
 <?php
-    require_once '../config/conexao.php';
-    if(!isset($_GET['acao'])) $acao="listar";
-    else $acao = $_GET['acao'];
-    if($acao=="listar"){
-        $sql   = "SELECT * FROM classe";
-        $query = $con->query($sql);
-        $registros = $query->fetchAll();
-        require_once '../template/cabecalho.php';
-        require_once 'lista_classe.php';
-        require_once '../template/rodape.php';
-    }else if($acao == "novo"){
-        require_once '../template/cabecalho.php';
-        require_once 'form_classe.php';
-        require_once '../template/rodape.php';
-    }else if($acao == "gravar"){
-        $registro = $_POST;
-        $sql = "INSERT INTO classe(nome) VALUES(:nome)";
-        $query = $con->prepare($sql);
-        $result = $query->execute($registro);
-        if($result){
-            header('Location: ./classe.php');
-        }else{
-            echo "Erro ao tentar inserir o registro";
+    require_once '../config/conexao.php';// conexão com o banco de dados
+    if(!isset($_GET['acao'])) $acao="listar";// se não existir a variável acao, então ela assume o valor listar
+    else $acao = $_GET['acao'];// se existir a variável acao, então ela assume o valor que ela recebe
+    if($acao=="listar"){// se a variável acao for igual a listar
+        $sql   = "SELECT * FROM classe";// selecionar todos os registros da tabela classe
+        $query = $con->query($sql);// executar a query
+        $registros = $query->fetchAll();// armazenar todos os registros em uma variável
+        require_once '../template/cabecalho.php';// chamar o template de cabecalho
+        require_once 'lista_classe.php';// chamar o template de lista de classe
+        require_once '../template/rodape.php';// chamar o template de rodape
+    }else if($acao == "novo"){// se a variável acao for igual a novo
+        require_once '../template/cabecalho.php';// chamar o template de cabecalho
+        require_once 'form_classe.php';// chamar o template de formulário de classe
+        require_once '../template/rodape.php';// chamar o template de rodape
+    }else if($acao == "gravar"){// se a variável acao for igual a gravar
+        $registro = $_POST;// armazenar todos os dados do formulário em uma variável
+        $sql = "INSERT INTO classe(nome) VALUES(:nome)";// inserir os dados do formulário na tabela classe
+        $query = $con->prepare($sql);// preparar a query
+        $result = $query->execute($registro);// executar a query
+        if($result){// se o resultado for verdadeiro
+            header('Location: ./classe.php');// redirecionar para a página de classe
+        }else{// se o resultado for falso
+            echo "Erro ao tentar inserir o registro";// exibir mensagem de erro
         }
-    }else if($acao == "excluir"){
-        $id    = $_GET['id'];
-        $sql   = "DELETE FROM classe WHERE id = :id";
-        $query = $con->prepare($sql);
-        $query->bindParam(':id', $id);
-        $result = $query->execute();
-        if($result){
-            header('Location: ./classe.php');
-        }else{
-            echo "Erro ao tentar remover o registro de id: " . $id;
+    }else if($acao == "excluir"){// se a variável acao for igual a excluir
+        $id    = $_GET['id'];// armazenar o id do registro que será excluído
+        $sql   = "DELETE FROM classe WHERE id = :id";// excluir o registro da tabela classe
+        $query = $con->prepare($sql);// preparar a query
+        $query->bindParam(':id', $id);// vincular o id do registro ao id do formulário
+        $result = $query->execute();// executar a query
+        if($result){// se o resultado for verdadeiro
+            header('Location: ./classe.php');// redirecionar para a página de classe
+        }else{// se o resultado for falso
+            echo "Erro ao tentar remover o registro de id: " . $id;// exibir mensagem de erro
         }
-    }else if($acao == "buscar"){
-        $id    = $_GET['id'];
-        $sql   = "SELECT * FROM classe WHERE id = :id";
-        $query = $con->prepare($sql);
-        $query->bindParam(':id', $id);
-        $query->execute();
-        $registro = $query->fetch();
-        require_once '../template/cabecalho.php';
-        require_once 'form_classe.php';
-        require_once '../template/rodape.php';
-    }else if($acao == "atualizar"){
-        $sql   = "UPDATE classe SET nome = :nome WHERE id = :id";
-        $query = $con->prepare($sql);
-        $query->bindParam(':id', $_GET['id']);
-        $query->bindParam(':nome', $_POST['nome']);
-        $result = $query->execute();
-        if($result){
-            header('Location: ./classe.php');
-        }else{
-            echo "Erro ao tentar atualizar os dados";
+    }else if($acao == "buscar"){// se a variável acao for igual a buscar
+        $id    = $_GET['id'];// armazenar o id do registro que será buscado
+        $sql   = "SELECT * FROM classe WHERE id = :id";// buscar o registro da tabela classe
+        $query = $con->prepare($sql);// preparar a query
+        $query->bindParam(':id', $id);// vincular o id do registro ao id do formulário
+        $query->execute();// executar a query
+        $registro = $query->fetch();// armazenar o registro encontrado em uma variável
+        require_once '../template/cabecalho.php';// chamar o template de cabecalho
+        require_once 'form_classe.php';// chamar o template de formulário de classe
+        require_once '../template/rodape.php';// chamar o template de rodape
+    }else if($acao == "atualizar"){// se a variável acao for igual a atualizar
+        $sql   = "UPDATE classe SET nome = :nome WHERE id = :id";// atualizar o registro da tabela classe
+        $query = $con->prepare($sql);// preparar a query
+        $query->bindParam(':id', $_GET['id']);// vincular o id do registro ao id do formulário
+        $query->bindParam(':nome', $_POST['nome']);// vincular o nome do registro ao nome do formulário
+        $result = $query->execute();// executar a query
+        if($result){// se o resultado for verdadeiro
+            header('Location: ./classe.php');// redirecionar para a página de classe
+        }else{// se o resultado for falso
+            echo "Erro ao tentar atualizar os dados";// exibir mensagem de erro
         }
     }
 ?>
