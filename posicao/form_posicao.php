e5f87afb9e0638fbc605379fadc3cc1026eee793
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2022-03-16 17:06:04 -0300

    Update form_posicao.php

--- posicao/form_posicao.php
+++ posicao/form_posicao.php
@@ -1,6 +1,6 @@
 <?php
-    if(isset($registro)) $acao = "posicao.php?acao=atualizar&id=".$registro['id'];
-    else $acao = "posicao.php?acao=gravar";
+    if(isset($registro)) $acao = "posicao.php?acao=atualizar&id=".$registro['id'];// se existir o registro
+    else $acao = "posicao.php?acao=gravar";// se não existir o registro
 ?>
 <div class="container">
     <form class="" action="<?php echo $acao; ?>" method="post">
